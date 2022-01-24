4102fbf89092c51ce208d3898c3e99e8c00a1576
drhtka <drhtka@gmil.com>
2022-01-24 06:30:40 +0200

    Initial commit

---
+++ shop/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from shop.models import Category, Promo, Brands
+
+
+# @admin.register(Brands)
+class BrandsAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name', 'get_img',)
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ('name',)
+    fields = ('name', 'slug', 'image', 'get_img',)
+    readonly_fields = ('get_img',)
+
+    def get_img(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="80px"')
+        else:
+            return 'нет картинки'
+
+    get_img.short_description = 'Миниатюра'
+admin.site.register(Brands, BrandsAdmin)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name', 'get_img', 'id',)
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ('name',)
+    fields = ('name', 'slug', 'image', 'get_img',)
+    readonly_fields = ('get_img',)
+
+    def get_img(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="80px"')
+        else:
+            return 'нет картинки'
+
+    get_img.short_description = 'Миниатюра'
+
+@admin.register(Promo)
+class PromoAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'promo_type', 'get_img', 'start_time', 'end_time', 'id', 'available',)
+    prepopulated_fields = {'slug': ('promo_type',)}
+    list_editable = ('promo_type',  'end_time', 'available',)
+    fields = ('promo_type', 'slug', 'image', 'start_time', 'end_time', 'get_img',)
+    readonly_fields = ('get_img',)
+
+    def get_img(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="80px"')
+        else:
+            return 'нет картинки'
+
+    get_img.short_description = 'Миниатюра'
---
+++ shop/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+class Brands(models.Model):
+    class Meta:
+        ordering = ('-name',)
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренд'
+
+    name = models.CharField('Бренды', max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
+    image = models.ImageField('Картинка', upload_to='brands/')
+
+    def __str__(self):
+        return self.name
+
+
+class Promo(models.Model):
+    class Meta:
+        verbose_name = 'Реклама акции'
+        verbose_name_plural = 'Реклама акции'
+
+    slug = models.SlugField(max_length=64, unique=True)
+    image = models.ImageField('Картинка', upload_to='promo/')
+    promo_type = models.CharField('Тип рекламы', max_length=128)
+    description = models.TextField('Опсание', )
+    start_time = models.DateField('Начало', auto_now_add=True)
+    end_time = models.DateField('Конец', null=True)
+    available = models.BooleanField('Активно', default=True)
+
+    def __str__(self):
+        return self.promo_type
+
+class Category(models.Model):
+    class Meta:
+        ordering = ('-name',)
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+
+    name = models.CharField('Название', max_length=128)
+    slug = models.SlugField(max_length=64, unique=True)
+    image = models.ImageField('Картинка', upload_to='category/')
+
+    def __str__(self):
+        return self.name
